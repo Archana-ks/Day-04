9cc3ac3f318b7a3bf72d6e063e1d4af57f0276eb
Archana-ks <122082888+Archana-ks@users.noreply.github.com>
2023-01-19 10:28:48 -0500

    Add files via upload

---
+++ v3.js
@@ -0,0 +1,15 @@
+//1st step: create a XHR object
+var request = new XMLHttpRequest();
+//2nd Step: API URL
+//.open method
+//2 parameters
+request.open("GET","https://restcountries.com/v3.1/all");
+//3rd step:establishing the bridge
+request.send();
+//4th step: once the data successfully received from server
+//onload is an event
+//when we use: once the data /server is showing 200 status code 
+request.onload=function(){
+var result=JSON.parse(request.response);
+console.log(result);
+}
